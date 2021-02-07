01a8e333a73aa13b8532c5b2d00c5032c2f920dc
Валерий Проскуряков <79242258075@yandex.ru>
2021-02-07 14:43:14 +1000

    Initial commit

---
+++ Readability Score/task/src/readability/Main.java
@@ -0,0 +1,17 @@
+package readability;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        if(text.length()>100){
+            System.out.print("HARD");
+        }else {
+            System.out.print("EASY");
+        }
+
+
+    }
+}
